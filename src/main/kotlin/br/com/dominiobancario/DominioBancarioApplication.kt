c5d7908a73897f8d98931ff7be0061af6de912f3
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-16 10:46:36 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/dominiobancario/DominioBancarioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dominiobancario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DominioBancarioApplication
+
+fun main(args: Array<String>) {
+    runApplication<DominioBancarioApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dominiobancario/DominioBancarioApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dominiobancario
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DominioBancarioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
